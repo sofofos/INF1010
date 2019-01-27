2ea858899ee3de6e01ecb007e724017c7bc34725
sofos-alvagonza <sofia.alva.gonza@gmail.com>
2019-01-27 05:18:05 -0500

    modif de menu.cpp

--- TP1/TP1/Menu.cpp
+++ TP1/TP1/Menu.cpp
@@ -7,42 +7,97 @@
 */
 
 #include "Menu.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-Menu::Menu(){
+using namespace std;
 
-    unsigned int capacite_ = MAXPLAT;
-    Plat** listePlats_ ;
-    unsigned int nbPlats_;
-    TypeMenu type_;
+Menu::Menu()
+{
+
+    capacite_ = MAXPLAT;
+    nbPlats_= 0;
+    type_ = Matin;
+    for (int i = 0; i < MAXPLAT; i++)
+    {
+        listePlats.push_back(new Plat());
+    }
 
 }
 
 Menu::Menu(string fichier, TypeMenu type){
+    type_ = type;
+
+    string strMenu;
+
+    ifstream ficMenu;
+    ficMenu.open("polyFood.txt");
+
+    while(!ws(ficMenu).eof()){
+        getline(ficMenu,strMenu);
+
+        if(strMenu[0] == '-'){
+            strMenu = strMenu.substr(1);
+            //if(strMenu == 'MATIN'){}
+        }
+        // switch (TypeMenu) {
+        //     case 'Matin': cout << "   ";
+        //     case 'Midi' : cout << "   ";
+        //     case 'Soir' : cout << "   ";
+        // }
+    }
+}
 
 
 }
 
 int Menu::getNbPlats() const {
+    return nbPlats_;
 
-}g
- void Menu::afficher() {
+}
+ void Menu::afficher() const {
+    cout << "Voici son menu " << endl;
+
 
 }
 
 Plat* Menu::trouverPlat(string &nom) {
+    for (int i = 0; i < listePlats.size(); i++)
+    {
+        if (listePlats.at(i) -> getNom() == nom)
+        {
+            return listePlats.at(i);
+        }
+    }
 
+    Plat* plat = new Plat();
+    return plat;
 }
 
 void Menu::ajouterPlat(Plat &plat) {
+    listePlats.push_back(&plat);
+
 
 }
 
 void Menu::ajouterPlat(string &nom, double montant, double cout) {
+    listePlats.push_back(new Plat(nom,montant,cout));
 
 }
 
 bool Menu::lireMenu(string &fichier) {
 
+    string strMenu;
+
+    ifstream ficMenu;
+    ficMenu.open("polyFood.txt");
+
+    while(!ws(ficMenu).eof()){
+        getline(ficMenu,strMenu);
+
+        if(strMenu[0] == '-'){
+            strMenu = strMenu.substr(1); // lire doit storer dans listePlats
+        }
+    }
 }
-
-
