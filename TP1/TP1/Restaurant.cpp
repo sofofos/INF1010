b8cb723e9859ba40de89d46e15f2d2b3f77e26c8
Sofos <sofia.alva.gonza@gmail.com>
2019-01-31 11:25:27 -0500

    delete 2nd version of restaurant.cpp

--- TP1/TP1/Restaurant.cpp
+++
@@ -1,9 +0,0 @@
-/*
-* Titre : Menu.cpp - Travail Pratique #1
-* Date : 24 Janvier 2019
-* Auteur : Sofia Alvarez (1894026) & Justin de Meulemeester(1897007)
-*/
-
-#include "Restaurant.h"
-#include <string>
-
