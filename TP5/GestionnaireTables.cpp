871a931ddd31443bbbf21bbb24074916c665dce1
sofos-alvagonza <sofia.alvarez@polymtl.ca>
2019-04-07 15:03:02 -0400

    fini gestionnaireTables

--- TP5/GestionnaireTables.cpp
+++ TP5/GestionnaireTables.cpp
@@ -8,8 +8,26 @@
 #include "GestionnaireTables.h"
 #include "LectureFichierEnSections.h"
 
-Table* getTable(int id) {
+Table* GestionnaireTables::getTable(int id) const{
+	return *find(conteneur_.begin(), conteneur_.end(), id);
 
+}
+
+Table* GestionnaireTables::getMeilleureTable(int tailleGroupe) const {
+	Table* meilleureTable = nullptr;
+	set<Table*>::iterator itr = conteneur_.begin();
+	for (itr; itr != conteneur_.end(); itr++) {
+		Table* table = *itr;
+		if (table->getNbPlaces >= tailleGroupe && table->estOccupee() == false) {
+			if (meilleureTable == nullptr) {
+				meilleureTable = table;
+			}
+			else if (table->getNbPlaces() < table->getNbPlaces) {
+				meilleureTable = table;
+			}
+		}
+	}
+	return meilleureTable;
 }
 
 void GestionnaireTables::lireTables(const string& nomFichier)
@@ -22,4 +40,13 @@ void GestionnaireTables::lireTables(const string& nomFichier)
 		ajouter(new Table(id, nbPlaces));
 	}
 }
+
+void GestionnaireTables::afficherTables(ostream& os) const {
+	set<Table*>::iterator itr = conteneur_.begin();
+	for (itr; itr != conteneur_.end(); itr++) {
+		os << **itr;
+	}
+
+}
+
 
